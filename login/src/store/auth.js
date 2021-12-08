14b0ff1a5f88c47d1e9ca11cea18a313a5904629
Tomonori Mine <tomonori.mine.biz@gmail.com>
2021-12-08 22:47:51 +0900

    ログイン機能の勉強用コメント追加

--- login/src/store/auth.js
+++ login/src/store/auth.js
@@ -1,12 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { login as loginApi } from "../api/auth";
+import { createSlice } from '@reduxjs/toolkit';
+import { login as loginApi } from '../api/auth';
 
 const initialState = {
   user: null,
 };
 
 const slice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     setUser: (state, action) => {
@@ -17,11 +17,20 @@ const slice = createSlice({
 
 export default slice.reducer;
 
+/**
+ * 認証済みか確認するセレクター
+ * useSelectorフックでstateにアクセスするときに使う
+ */
 export const isAuthSelector = (state) => state.auth.user !== null;
 
+/**
+ * ログイン機能
+ * useDispatchでdispatchを使用するときに使う
+ */
 export function login(username, password) {
   return async function (dispatch) {
     const user = await loginApi(username, password);
+    // ログイン後にユーザー情報をストアに格納する
     dispatch(slice.actions.setUser(user));
   };
 }
